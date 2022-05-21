76bacbdc305fa0e8d8de39628fd4047db856ad9a
Lakshmi Manoj <lakshmi.kanagala@sstech.us>
2022-05-20 21:18:40 -0500

    Added function to close Keyboard.

--- app/src/main/java/com/manukanagala/everydaycalculations/MainActivity.kt
+++ app/src/main/java/com/manukanagala/everydaycalculations/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Add Key Listeners for Edit text
-        binding.originalPriceEditText.setOnKeyListener { view, keyCode, _ ->  handleKeyEvent(view, keyCode)}
-        binding.discountPercentageEditText.setOnKeyListener { view, keyCode, _ ->  handleKeyEvent(view, keyCode)}
+        binding.originalPriceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.discountPercentageEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
 
         //Add Click listener for the button
         binding.calculate.setOnClickListener { calculateDiscount() }
@@ -33,8 +33,8 @@ class MainActivity : AppCompatActivity() {
      * This method closes the key board once the user clicks on the Enter button
      * Also resets the Text views once the user clicks on Delete button
      */
-    private fun handleKeyEvent(view: View, keyCode: Int): Boolean{
-        if (keyCode == KeyEvent.KEYCODE_ENTER){
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
 
             // Hide the Keyboard once the user clicks on Enter button
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Reset the text view on Delete button click
-        if (keyCode == KeyEvent.KEYCODE_DEL){
+        if (keyCode == KeyEvent.KEYCODE_DEL) {
             binding.discountedAmount.text = ""
             binding.finalAmount.text = ""
         }
@@ -54,13 +54,16 @@ class MainActivity : AppCompatActivity() {
      * This method calculates the discounted price by capturing the data from Edit Text
      * Also, sets the text views to the discounted and final amounts
      */
-    private fun calculateDiscount(){
+    private fun calculateDiscount() {
+
+        //Hide keyboard on Calculate button Click
+        closeKeyBoard()
 
         // Capture data from Edit Text
         val originalAmountInEditText = binding.originalPriceEditText.text.toString()
         val originalAmount = originalAmountInEditText.toDoubleOrNull()
         // Throw an error if the error value was entered
-        if (originalAmount == null){
+        if (originalAmount == null) {
             binding.originalPriceEditText.error = getString(R.string.enter_valid_value)
             binding.finalAmount.text = ""
             return
@@ -70,16 +73,16 @@ class MainActivity : AppCompatActivity() {
         val discountInEditText = binding.discountPercentageEditText.text.toString()
         val discountPercentage = discountInEditText.toDoubleOrNull()
         // Throw an error if the error value was entered
-        if (discountPercentage == null){
+        if (discountPercentage == null) {
             binding.discountPercentageEditText.error = getString(R.string.enter_valid_value)
             binding.discountedAmount.text = ""
             return
         }
 
-        val discount = discountPercentage/100
+        val discount = discountPercentage / 100
 
         // Calculate the Discount and Original Amount
-        val discountAmount = discount*originalAmount
+        val discountAmount = discount * originalAmount
         val finalAmount = originalAmount - discountAmount
 
         // Format the amount to the current currency
@@ -90,5 +93,17 @@ class MainActivity : AppCompatActivity() {
         binding.discountedAmount.text = formattedDiscountAmount
         binding.finalAmount.text = formattedFinalAmount
 
+    }
+
+    /**
+     * This below method is used to close the Key board
+     */
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            // Hide the Keyboard once the user clicks on Enter button
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
