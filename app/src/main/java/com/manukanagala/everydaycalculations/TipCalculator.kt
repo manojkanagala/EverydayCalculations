e3b67a987bb12f7d3a5ff168ebb39e0c9b9ead7c
Lakshmi Manoj <lakshmi.kanagala@sstech.us>
2022-05-28 09:34:29 -0500

    Added a new activity.

---
+++ app/src/main/java/com/manukanagala/everydaycalculations/TipCalculator.kt
@@ -0,0 +1,11 @@
+package com.manukanagala.everydaycalculations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TipCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tip_calculator)
+    }
+}
